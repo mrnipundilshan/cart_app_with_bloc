5ba2b4f190b99a993a630966de4cbd2f1421d33a
Nipun Dilshan <nipundilshan991@gmail.com>
2025-05-13 21:47:22 +0530

    Add home event classes for product wishlist and cart interactions

--- lib/features/home/bloc/home_event.dart
+++ lib/features/home/bloc/home_event.dart
@@ -2,3 +2,11 @@ part of 'home_bloc.dart';
 
 @immutable
 sealed class HomeEvent {}
+
+class HomeProductWishListButtonClickedEvent extends HomeEvent {}
+
+class HomeProductCartButtonClicked extends HomeEvent {}
+
+class HomeWishListButtonNavgateEvent extends HomeEvent {}
+
+class HomeCartButtonNavigateEvent extends HomeEvent {}
